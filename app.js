7e643737fa8fe61fc262303a9b96dd2e286433eb
PCWitcher <pcwitcher.ie@gmail.com>
2020-01-19 15:44:49 +0100

    center watermark

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const Jimp = require('jimp');
 const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
     const image = await Jimp.read(inputFile);
     const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-    image.print(font, 10, 10, text);
+    const textData = {
+        text,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    };
+
+    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
     image.quality(100).write(outputFile);
 };
 
