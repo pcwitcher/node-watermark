a171bc1f7221df23a078344ef35baccb7b4b46ff
PCWitcher <pcwitcher.ie@gmail.com>
2020-01-19 16:02:17 +0100

    add image

--- app.js
+++ app.js
@@ -14,3 +14,18 @@ const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
 };
 
 addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'Hello world')
+
+const addImageWatermarkToImage = async function (inputFile, outputFile, watermarkFile) {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+    image.composite(watermark, x, y, {
+        mode: Jimp.BLEND_SOURCE_OVER,
+        opacitySource: 0.5,
+    });
+    image.quality(100).write(outputFile);
+};
+
+addImageWatermarkToImage('./test.jpg', './test-with-watermark2.jpg', './logo.png');
